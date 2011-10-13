bd85aa0039eb9d9757aa223d7b0719c7b1a0c097
Jeff Stubler <brunsa2@gmail.com>
2011-10-12 21:55:19 -0500

    Add blank files for program structure
    Added blank files for program threads including test message in main thread

---
+++ chat/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Jeff Stubler
+ * CS 3841
+ * Lab 5 -- UNIX chat program
+ * 12 October 2011
+ *
+ * Main entry point for client, gathers arguments, starts threads, and acts as
+ * main controller for system
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello, World\n");
+    return 0;
+}
