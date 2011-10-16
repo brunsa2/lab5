8ebc140d8885a6b12d9c5bb8bf4f00d0ee7eabd0
Jeff Stubler <brunsa2@gmail.com>
2011-10-16 18:45:49 -0500

    Add thread files for each socket connection

---
+++ chatd/src/socketthread.h
@@ -0,0 +1,13 @@
+/*
+ * Jeff Stubler
+ * CS 3841
+ * Lab 5 -- UNIX chat program
+ * 15 October 2011
+ *
+ * Main thread for each socket
+ */
+
+#ifndef SOCKET_THREAD
+#define SOCKET_THREAD
+
+#endif
