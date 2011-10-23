6b5b09b38bb277d315cbd275bbe0ae21d88115fd
Matt Edwards <edwardsmr@msoe.edu>
2011-10-23 13:48:32 -0500

    Modified the ui.c file to check for and add all information. Will not compile as placeholders are used for function names

--- chat/src/ui.c
+++ chat/src/ui.c
@@ -14,82 +14,116 @@ WINDOW * w_input;
 WINDOW * w_user;
 WINDOW * w_time;
 
+extern login_info login;
+
 void *ui_thread(void *argument) {
-    return NULL;
-}
+    char str[80];
+    char tick[10];
+    char new_user[20];
+    char[20] time;
+    char[20] user;
+    char[80] message;
+    int row, col, y, x, chat_line, user_line, buffer;
 
-int main()
-{
-	char user[20];
-	char str[80];
-	int row,col;				/* to store the number of rows and *
-					 * the number of colums of the screen */
-	int y, x, chat_line, user_line;
+    initscr();				/* start the curses mode */
+    start_color();
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
+    getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
 
-	initscr();				/* start the curses mode */
-	start_color();
-	keypad(stdscr, TRUE);
-	init_pair(1, COLOR_CYAN, COLOR_BLACK);
-	getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+    attron(COLOR_PAIR(1));
+    printw("\t\tType 'EXIT' to quit the program.");
+    refresh();
+    attroff(COLOR_PAIR(1));
 
-	attron(COLOR_PAIR(1));
-	printw("Please enter your username:\t\tPress Ctrl+C to exit");
-	refresh();
-	attroff(COLOR_PAIR(1));
+    x=col/1.38;
+    y=row-6;
+    user_line = 1;
+    chat_line = 1;
 
-	x=col/1.38;
-	y=row-6;
-	user_line = 1;
-	chat_line = 1;
+    
+    w_chat = newwin(y, x, 1, 1);
+    w_input = newwin(3, x, y+1, 1);
+    w_user = newwin(y, col/6, 1, x+2);
+    w_time = newwin(3, col/6, y+1, x+2);
 
-	w_chat = newwin(y, x, 1, 1);
-	w_input = newwin(3, x, y+1, 1);
-	w_user = newwin(y, col/6, 1, x+2);
-	w_time = newwin(3, col/6, y+1, x+2);
+    /*Create panels for each window*/
+    box(w_chat, ACS_VLINE, ACS_HLINE);
+    box(w_input, ACS_VLINE, ACS_HLINE);
+    box(w_user, ACS_VLINE, ACS_HLINE);
+    box(w_time, ACS_VLINE, ACS_HLINE);
 
-	box(w_chat, ACS_VLINE, ACS_HLINE);
-	box(w_input, ACS_VLINE, ACS_HLINE);
-	box(w_user, ACS_VLINE, ACS_HLINE);
-	box(w_time, ACS_VLINE, ACS_HLINE);
+    /*TODO: Display ticking time in the bottom right panel
+    * TODO: Displaying all users in chat room
+    * TODO: Displaying received input
+    * TODO: Displaying sent input
+    */
+    mvwprintw(w_user, user_line, 1, "%s", login.username);
+    user_line++;
 
-	wmove(w_input, 1, 1);
-	wgetstr(w_input, user);
-	wclear(w_input);
-	box(w_input, ACS_VLINE, ACS_HLINE);
-
-	mvwprintw(w_user, user_line, 1, "%s", user);
-	user_line++;
-
-	while(true){
+    while(true){
+        refresh_all();
+        
+        if(has_keys() == true){
+            buffer = 0;
+	    //placeholder until keyboard.c code is implemented
+            while(buffer < 80 && kb_buffer[buffer] != "\r\n") {
+		mvwaddch(w_input,1, buffer+1, kb_buffer[buffer]);
+		strcat(str,kb_buffer[buffer]);
+                buffer++;
 		refresh_all();
+            }
+        }
 
-		wmove(w_input, 1, 1);
+        if(strcmp(str, "EXIT") == 0){
+            endwin();
+            return 0;
+        }
 
-		wgetstr(w_input, str);
+        if(chat_line > 28){
+            chat_line = 1;
+            wclear(w_chat);
+            box(w_chat, ACS_VLINE, ACS_HLINE);
 
-		if(chat_line > 28){
-			chat_line = 1;
-			wclear(w_chat);
-			box(w_chat, ACS_VLINE, ACS_HLINE);
+            refresh_all();
+        }
 
-			refresh_all();
-		}
-
-		mvwprintw(w_chat, chat_line, 1, "<%s> %s", user, str);
-		chat_line++;
-		wclear(w_input);
-		box(w_input, ACS_VLINE, ACS_HLINE);
+	//placeholder
+	if(has_new_user() == true){
+	    strcpy(new_user, get_user());
+	    mvwprintw(w_user, user_line, 1, "%s", new_user);
+	    user_line++;
+	    refresh_all();
 	}
 
-	endwin();
+	//placeholder until has_message is implemented in some form
+	if(has_message() == true){
+	    //placeholders
+	    strcpy(time, get_time());
+	    if(get_user() == NULL){
+		strcpy(user, "SYSTEM");
+	    } else{
+		strcpy(user, get_user());
+	    }
+	    strcpy(message, get_message());
+	
+	    mvwprintw(w_chat, chat_line, 1, "[%s]<%s> %s", time, user, message);  
+	    chat_line++;  
+	}
+	
+	//placeholder
+	strcpy(tick, get_tick());
+	mvwprintw(w_time, 1, 1, "%i", tick);
+    }
 
-	return 0;
+    endwin();
+
+    return 0;
 }
 
 void refresh_all(){
-	wrefresh(w_chat);
-	wrefresh(w_input);
-	wrefresh(w_user);
-	wrefresh(w_time);
-	refresh();
+    wrefresh(w_chat);
+    wrefresh(w_input);
+    wrefresh(w_user);
+    wrefresh(w_time);
+    refresh();
 }
